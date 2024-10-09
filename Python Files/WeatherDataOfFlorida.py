f5406f7d95ce9291578ab32ab800620818b108e7
Jake Harris <jmh00@comcast.net>
2024-10-09 16:11:40 -0400

    V1.4 Automatic Script Updates The program has been changed to now update at hourly intervals. The amount of rainfall per hour has also been included in the weather information. This information is displayed in inches per hour.

--- Python Files/WeatherDataOfFlorida.py
+++ Python Files/WeatherDataOfFlorida.py
@@ -4,6 +4,7 @@ import time
 # OpenWeatherAPI
 api_key = "1dd5aa3837d3ec1a46e9a72a65841d15"
 
+# function to grab city name's lat and long coordinates
 def location(city_name):
     location_request = requests.get(f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit={1}&appid={api_key}")
     loc_data = location_request.json()
@@ -11,7 +12,6 @@ def location(city_name):
         return loc_data[0]
     else:
         print("City not found.")
-
 
 
 # function to pull live weather data from OpenWeatherAPI
@@ -25,34 +25,45 @@ def get_weather_data(lat, lon):
         return data
     else:
         print('Error: ', data_request.status_code)
-        return None
+        
         
         
 # function to display relevant weather data of chosen location
 def display_weather(data):
 
+    # weather info to grab
     city = data['name']
     temperature = data['main']['temp']
     wind_speed = data['wind']['speed']
+    weather = data['weather'][0]['main']
     weather_desc = data['weather'][0]['description']
-
+    rain_amount_in_mm = data['rain']['1h']
+    rain_in_inches = rain_amount_in_mm / 25.4
+    rain_per_hour = round(rain_in_inches, 2)
+    
+    # weather info to display
     print(f"City: {city}")
     print(f"Temperature: {temperature} F")
-    print(f"Weather: {wind_speed} mph")
+    print(f"Weather: {weather}")
+    print(f"Rain per hour: {rain_per_hour} inches")
+    print(f"Wind Speed: {wind_speed} mph")
     print(f"Description: {weather_desc}")
-
 
 
 # user input
 city_name = input("Please enter the city you'd like weather data for: ")
 
+# grabbing lat and long coords of city name
 location_data = location(city_name)
 if location_data:
     lat = location_data['lat']
     lon = location_data['lon']
 
-
-weather_data = get_weather_data(lat, lon)
-display_weather(weather_data)
-
     
+# the script is programmed to update every hour
+while True:
+    weather_data = get_weather_data(lat, lon)
+    display_weather(weather_data)
+
+    print("\nWaiting for the next update...\n")
+    time.sleep(3600)   
